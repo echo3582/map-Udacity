c2c6708a7716c12c2c476ef477861e02c293e05c
echo3582 <qianlu3582@163.com>
2019-03-05 17:47:37 +0800

    Updates

---
+++ precache-manifest.d666d8bc44a818a0beb058655f07d993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3861e98ec3e0ac7b92d",
+    "url": "/map-Udacity/static/js/runtime~main.e3861e98.js"
+  },
+  {
+    "revision": "d83652fe28910c7626b6",
+    "url": "/map-Udacity/static/js/main.d83652fe.chunk.js"
+  },
+  {
+    "revision": "b8b731381d9a7bc2f17d",
+    "url": "/map-Udacity/static/js/1.b8b73138.chunk.js"
+  },
+  {
+    "revision": "d83652fe28910c7626b6",
+    "url": "/map-Udacity/static/css/main.a6ccf5a3.chunk.css"
+  },
+  {
+    "revision": "f05db04e3f3847d97828f6343b77e255",
+    "url": "/map-Udacity/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/map-Udacity/precache-manifest.7bd3e55641807a3f70e62531ed1ae39c.js"
+  "/map-Udacity/precache-manifest.d666d8bc44a818a0beb058655f07d993.js"
 );
 
 workbox.clientsClaim();
